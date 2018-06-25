1d0f9a0835cf61dcac83a747de07c07a3a08ad06
Jens Hagen <hagen-j@outlook.com>
2018-06-25 07:40:35 +0200

    # added unique_ptr to the sample. cleaned up file. removed unnessesary structs.

--- examples/example_STL/example_STL.cpp
+++ examples/example_STL/example_STL.cpp
@@ -2,29 +2,10 @@
 // include hsa.h
 #include <hsa.h>
 
+#include <functional>
 #include <map>
 #include <memory>
 #include <vector>
-
-
-template<class T>
-using StdVector = std::vector<T, STLAllocatorWrapper<void>>;
-
-struct DataTest2
-{
-	int data_ = 0;
-	int other_data_ = 0;
-};
-
-struct DataTest
-{
-	int data_ = 0;
-	int other_data_ = 0;
-	int data1_ = 0;
-	int other_data1_ = 0;
-	int data2_ = 0;
-	int other_data2_ = 0;
-};
 
 int main( int arg_n, char** arg_s )
 {
@@ -37,7 +18,7 @@ int main( int arg_n, char** arg_s )
 
 	// give wrapper to vector.
 	// again it does not matter what template paramerter is use for the STLAllocatorWrapper
-	// but i recommend using void to avoid unnesserary confusion.
+	// but I recommend using void to avoid unnesserary confusion.
 	std::vector<int, STLAllocatorWrapper<void>> custom_allocator_vector = std::vector<int, STLAllocatorWrapper<void>>( std_alloc );
 
 	// use std::vector as normal
@@ -53,7 +34,23 @@ int main( int arg_n, char** arg_s )
 	// instead of std::make_shared.
 	// it is recommended to use std::allocate_shared instead of using the std::shared_ptr constructor.
 	// the constructor required a deallocator which is supplied by std::allocate_shared
-	std::shared_ptr<DataTest2> dd2_shptr = std::allocate_shared<DataTest2>( std_alloc, DataTest2() );
+	std::shared_ptr<int> dd2_shptr = std::allocate_shared<int>( std_alloc, int() );
+	
+	// to use a custom allocator with unique pointers you have to do a lot more work.
+	// for this to work you first have to give the unique_ptr a Destructor function.
+	// the easiest thing you can do is use std::function, #include <functional>
+	// the std::function needs a single parameter: Type*
+	std::unique_ptr<int, std::function<void( int* )>> dd_uptr = {
+		new( free_list_alloc.Allocate( sizeof( int ) ) ) int(),	// by allocating memory with the custom allocator and using placement new to construct the object.
+		std::bind(															// using bind to group a lambda and 2 parameters together. this will become the Destructor function.
+			[]( int* arg_ptr, STLAllocatorWrapper<void> arg_alloc )
+				{
+					arg_alloc.deallocate( arg_ptr, 0 );						// use custom allocator to deallocate the memory when destructor is called.
+				},
+			std::placeholders::_1,											// the first parameter is a placeholder. this is the first parameter and will be supplied when the lambda is called.
+			std_alloc														// the second parameter is supplied at the creation of the lambda this is your custom allocator.
+			)
+		};
 
 
 	std::getchar();
